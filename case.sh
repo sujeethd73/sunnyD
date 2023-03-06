a20fc834bc19a97b06e608e0a9a7ffc92beb8935
sujeethd73 <sujithd587@gmail.com>
2023-03-06 15:46:16 +0530

    my first push

--- case.sh
+++ case.sh
@@ -10,6 +10,6 @@ case $ACTION in
        echo "stoping xyz service"
        ;;
     *)
-        echo "\e[31m valid options are either start or stop \e[0m"
+        echo -e "\e[31m valid options are either start or stop \e[0m"
 
 esac
