8edaafe710af754a9cf7728c076da5a40210f1be
sujeethd73 <sujithd587@gmail.com>
2023-03-06 15:55:30 +0530

    my first push

--- case.sh
+++ case.sh
@@ -5,11 +5,14 @@ ACTION=$1
 case $ACTION in
    start)
        echo "starting xyz service"
+       exit 0
        ;;
     stop)
        echo "stoping xyz service"
+       exit 1
        ;;
     *)
         echo -e "\e[31m valid options are either start or stop \e[0m"
+        exit 2
 
 esac
