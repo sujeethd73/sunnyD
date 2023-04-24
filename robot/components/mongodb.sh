2272aaf8af5f0238e35dbd802328a6d6b4f32942
sujeethd73 <sujithd587@gmail.com>
2023-04-24 17:59:19 +0530

    My first push

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 set -e
 
 # verify the script is been executed as root user or not
 
 COMPONENT=mongodb
+
+source components/common.sh
 
 echo -n "configuring the repo:"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
