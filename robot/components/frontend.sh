0e441f8406146aa1b8c876883ca356cfe30ab92c
sujeethd73 <sujithd587@gmail.com>
2023-04-24 15:45:33 +0530

    My first push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -5,6 +5,6 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] ; then
   echo -e "\e[31m you must have to run this script as a room user or sudo previlage \e[0m"
   exit 1
-fi 
+fi
+echo "installing nginx" 
  yum install nginx -y &>> /tmp/frontend.log
- 
