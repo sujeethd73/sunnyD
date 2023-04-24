222cbfbdf0d4ee7d1858ea4c09e85e5f0449441c
sujeethd73 <sujithd587@gmail.com>
2023-04-25 01:19:47 +0530

    My first push

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -26,7 +26,7 @@ systemctl start mongod &>> $LOGFILE
 stat $?
 
 echo -n "download the schema:"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip" &>> $LOGFILE
 stat $?
 
 echo -n "injecting the schema:"
