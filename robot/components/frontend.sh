80fee18aa14ff6abee8c70f7816118d077d58592
sujeethd73 <sujithd587@gmail.com>
2023-04-24 15:35:33 +0530

    My first push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -3,6 +3,6 @@
 # verify the script is been executed as root user or not
 USERID=$(id -u)
 if [ $USERID -ne 0 ] ; then
-  echo "\e[31m you must have to run this script as a room user or sudo previlage \e[0m"
+  echo -e "\e[31m you must have to run this script as a room user or sudo previlage \e[0m"
 fi 
  yum install nginx -y
