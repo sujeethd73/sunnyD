d4f65e2a0054ef4aa194fdc98d3762a8893fb742
sujeethd73 <sujithd587@gmail.com>
2023-04-24 15:31:07 +0530

    My first push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "i am frontend component"
-
+# verify the script is been executed as root user or not
+USERID=$(id -u)
+if [ USERID -NE 0 ] ; then
+    echo "\e[31m you must have to run this script as a room user or sudo previlage \e[0m"
+fi 
+ yum install nginx -y
