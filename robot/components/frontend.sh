4985ecfcfeeb30865050dd2e5a94b106034e7222
sujeethd73 <sujithd587@gmail.com>
2023-04-24 17:08:14 +0530

    My first push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -3,29 +3,53 @@ set -e
 
 # verify the script is been executed as root user or not
 USERID=$(id -u)
+COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
+
+
 if [ $USERID -ne 0 ] ; then
   echo -e "\e[31m you must have to run this script as a room user or sudo previlage \e[0m"
   exit 1
 fi
-echo -n "installing nginx:" 
-yum install nginx -y &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
+
+stat() {
+if [ $1 -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
 else  
   echo -e "\e[31m failure \e[0m"
-fi 
+fi
+} 
+
+echo -n "installing nginx:"
+
+yum install nginx -y &>> $LOGFILE
+stat $?
+
 
 echo -n "downloading the component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-echo "performing the cleaning up:"
-rm -rf/usr/share/nginx/html/* &>> /tmp/frontend.log*  
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo -n"performing the cleaning up:"
+
+rm -rf/usr/share/nginx/html/* &>> $LOGFILE*
+stat $?
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n"unzipping the component:"
 
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx &>> /tmp/frontend.log
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $?
+
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+echo -n"configuring the revers proxy file:"
+
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+echo -n"starting the server:"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
