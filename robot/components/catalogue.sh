30521a06edd55c53c751251b341d5fee2356dec9
sujeethd73 <sujithd587@gmail.com>
2023-05-04 14:02:24 +0530

    My first push

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -7,7 +7,8 @@ APPUSER=roboshop
 
 NODEJS
 
-echo -e "\e[33m_________$COMPONENT installation completed_________\e[0m"
+echo -e "\e[33m_________$COMPONENT installation completed_________\e[0m
+
 
 
 
