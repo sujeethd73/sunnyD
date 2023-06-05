63b57ff527762b7052014ed74a203e0e6b6bbc73
sujeethd73 <sujithd587@gmail.com>
2023-06-05 18:02:24 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,11 +1,12 @@
 #!bin/bash
-
+COMPONENT=$1
 AMI_ID="$(aws ec2 describe-images --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-
-echo $AMI_ID
+SGID="$(aws ec2 describe-security-groups --filters Name=group-name,values=ds-73-add | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+echo "AMI ID used to launch instance : $AMI_ID"
+echo "SG ID used to launch instance : $SGID"
 
 #Devops-labImage-Centos7
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq .
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
 
 #aws ec2 run-instances \
    # --image-id ami-0abcdef1234567890 \
