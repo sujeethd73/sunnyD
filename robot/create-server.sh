75412327b65370ee3eeb40a3c1acf6e55b6aa5c2
sujeethd73 <sujithd587@gmail.com>
2023-06-05 19:55:52 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -12,7 +12,13 @@ echo "AMI ID used to launch instance : $AMI_ID"
 echo "SG ID used to launch instance : $SGID"
 
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=Persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=Persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].privateIpAddress')
+
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
+
+echo -n "Creatngthe DNS Record ********"
+aws route53 change-resource-record-sets --hosted-zone-id Z05153301U7YF8KV1K7QB --change-batch file:///dns.json | jq
+
 
 #aws ec2 run-instances \
    # --image-id ami-0abcdef1234567890 \
@@ -21,4 +27,4 @@ aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-gro
     #Devops-labImage-Centos7
 
 #if we want run or launch a spot instance
-# --instance-market-options "MarketType=Spot, SpotOptions={SpotInstanceType=Persistent,InstanceInterruptionBehavior=stop}"
+# --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=Persistent,InstanceInterruptionBehavior=stop}"
