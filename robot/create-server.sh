ec6115ac18a5b845bc72c89be9afadeb9d7ff50d
sujeethd73 <sujithd587@gmail.com>
2023-06-05 18:07:39 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -5,10 +5,11 @@ SGID="$(aws ec2 describe-security-groups --filters Name=group-name,values=ds-73-
 echo "AMI ID used to launch instance : $AMI_ID"
 echo "SG ID used to launch instance : $SGID"
 
-#Devops-labImage-Centos7
+
 aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
 
 #aws ec2 run-instances \
    # --image-id ami-0abcdef1234567890 \
    # --instance-type t2.micro \
     #--key-name MyKeyPair
+    #Devops-labImage-Centos7
