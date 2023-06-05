276b5683d9128544610b2d5b3c3b04e92e3828e6
sujeethd73 <sujithd587@gmail.com>
2023-06-05 16:36:39 +0530

    My first push

---
+++ robot/create-server.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+AMI_ID="$(aws ec2 describe-images --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq 'Images[].ImageId')"
+
+echo $AMI_ID
+
+#Devops-labImage-Centos7
