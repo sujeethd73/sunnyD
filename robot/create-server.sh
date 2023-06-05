d339eb95730d96cf69f43a9dc7247b25558b4f41
sujeethd73 <sujithd587@gmail.com>
2023-06-05 17:05:44 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -5,3 +5,9 @@ AMI_ID="$(aws ec2 describe-images --region us-east-1  --filters "Name=name,Value
 echo $AMI_ID
 
 #Devops-labImage-Centos7
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
+
+#aws ec2 run-instances \
+   # --image-id ami-0abcdef1234567890 \
+   # --instance-type t2.micro \
+    #--key-name MyKeyPair
