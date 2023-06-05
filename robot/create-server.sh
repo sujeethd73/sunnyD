76832bfd0409a2778a567a1758ae00079cd9ac52
sujeethd73 <sujithd587@gmail.com>
2023-06-05 20:02:28 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -16,7 +16,7 @@ PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro -
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
 
-echo -n "Creatngthe DNS Record ********"
+echo -n "Creating the DNS Record ********"
 aws route53 change-resource-record-sets --hosted-zone-id Z05153301U7YF8KV1K7QB --change-batch file:///tmp/dns.json | jq
 
 
