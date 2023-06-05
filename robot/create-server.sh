1dfebd4b16235ccf11fadf3ca267ddc1d98546ab
sujeethd73 <sujithd587@gmail.com>
2023-06-05 18:25:22 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -12,7 +12,7 @@ echo "AMI ID used to launch instance : $AMI_ID"
 echo "SG ID used to launch instance : $SGID"
 
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
 #aws ec2 run-instances \
    # --image-id ami-0abcdef1234567890 \
