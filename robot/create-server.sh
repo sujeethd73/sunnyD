07338649efb4441529c1948d19f313a4f286a484
sujeethd73 <sujithd587@gmail.com>
2023-06-05 23:34:24 +0530

    My first push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 COMPONENT=$1
+ZONE_ID="Z05153301U7YF8KV1K7QB"
+
 if [ -z "$1" ]; then
     echo -e "\e[31m component name is required \n example usage is: \n\t bash create-server.sh componentname \e[0m"
     exit 1
@@ -12,13 +14,23 @@ echo "AMI ID used to launch instance : $AMI_ID"
 echo "SG ID used to launch instance : $SGID"
 
 echo $COMPONENT
+create-server() {
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=Persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
 
 echo -n "Creating the DNS Record ********"
-aws route53 change-resource-record-sets --hosted-zone-id Z05153301U7YF8KV1K7QB --change-batch file:///tmp/dns.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/dns.json | jq
+}
 
+if [ "$1" == "all" ]; then
+    for component in frontend mongodb catalogue cart user; do
+        COMPONENT=$component
+        create-server
+    done
+else
+        create-server
+fi        
 
 #aws ec2 run-instances\
    # --image-id ami-0abcdef1234567890 \
